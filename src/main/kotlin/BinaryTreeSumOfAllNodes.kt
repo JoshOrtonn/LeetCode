a6d969b96b10f7ee2dd3251a27b8f19dc5662e6d
Josh Orton <joshorton34@hotmail.co.uk>
2024-11-19 16:14:28 +1100

    chore: count number of nodes

--- src/main/kotlin/BinaryTreeSumOfAllNodes.kt
+++ src/main/kotlin/BinaryTreeSumOfAllNodes.kt
@@ -24,4 +24,28 @@ class BinaryTreeSumOfAllNodes {
         // then backtrack to parent, calculate right hand subtree, and add itself passing back to parent
         // Recursively, essentially breaking it down into smaller problems.
     }
+
+    // To solve for the problem of counting the number of nodes in a binary tree.
+    // Simply follow similar practices,
+    // Consider at any node, it's number of nodes, is 1 + left subtree Height + right subtree height
+    // Left subtree height = left subtreeHeight + right subtree height + 1
+    // Until we reach leaf node, which should equal 1.
+    fun countNumberOfNodes(node: TreeNode?): Int {
+        // base case of passing an empty tree
+        if(node == null) return 0
+        // To save on call frames
+        // if we know the current node is the leaf we can just one as value
+        if(node.left == null && node.right == null) return 1
+
+        // Otherwise, we will get the left subtree's value, and the right subtree's value
+        val left = countNumberOfNodes(node.left)
+        val right = countNumberOfNodes(node.right)
+
+        // Increment the left subtree and right subtree values by one.
+        return left + right + 1
+
+        // Which will essentially work down to leaf node, return itself,
+        // then backtrack to parent, calculate right hand subtree, and add itself passing back to parent
+        // Recursively, essentially breaking it down into smaller problems.
+    }
 }
--- src/test/kotlin/BinaryTreeSumOfAllNodesTest.kt
+++ src/test/kotlin/BinaryTreeSumOfAllNodesTest.kt
@@ -25,4 +25,25 @@ class BinaryTreeSumOfAllNodesTest {
         }
         assertEquals(32, binaryTreeSumOfAllNodes.solve(treeNode4))
     }
+
+    @Test
+    fun shouldCountNumberOfNodes(){
+        val treeNode1 = TreeNode(1)
+        val treeNode3 = TreeNode(3)
+        val treeNode6 = TreeNode(6)
+        val treeNode9 = TreeNode(9)
+        val treeNode2 = TreeNode(2).apply {
+            left = treeNode1
+            right = treeNode3
+        }
+        val treeNode7 = TreeNode(7).apply {
+            left = treeNode6
+            right = treeNode9
+        }
+        val treeNode4 = TreeNode(4).apply {
+            left = treeNode2
+            right = treeNode7
+        }
+        assertEquals(7, binaryTreeSumOfAllNodes.countNumberOfNodes(treeNode4))
+    }
 }
