68624276f44b284e79b47b9307d8373989ced60a
Josh Orton <joshorton34@hotmail.co.uk>
2024-11-19 15:35:33 +1100

    chore: find max binary tree value

---
+++ src/main/kotlin/BinaryTreeMaxValue.kt
@@ -0,0 +1,28 @@
+package org.example
+
+
+class BinaryTreeMaxValue {
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+
+    fun solve(node: TreeNode?): Int {
+        // base case of passing an empty tree
+        if(node == null) return 0
+
+        // Base case if leaf node, return itself to parent.
+        if(node.left == null && node.right == null) return node.`val`
+
+        // Otherwise, we will get the left subtree's max Value, and the right subtree's max value
+        val left = solve(node.left)
+        val right = solve(node.right)
+
+        // And then confirm which one of left, right, or current value is the max value, and return that to parent.
+        return maxOf(maxOf(left, right), node.`val`)
+
+        // Which will essentially work down to leaf node, return itself,
+        // then backtrack to parent, calculate right hand subtree, and return max of left, right or current.
+        // then backtrack again.. Recursively, essentially breaking it down into smaller problems.
+    }
+}
---
+++ src/test/kotlin/BinaryTreeMaxValueTest.kt
@@ -0,0 +1,28 @@
+import org.example.BinaryTreeMaxValue
+import org.example.BinaryTreeMaxValue.TreeNode
+import org.junit.jupiter.api.Assertions.assertEquals
+import kotlin.test.Test
+
+class BinaryTreeMaxValueTest {
+    val binaryTreeMaxValue = BinaryTreeMaxValue()
+    @Test
+    fun shouldFindMaxNodeInTree(){
+        val treeNode1 = TreeNode(1)
+        val treeNode3 = TreeNode(3)
+        val treeNode6 = TreeNode(6)
+        val treeNode9 = TreeNode(9)
+        val treeNode2 = TreeNode(2).apply {
+            left = treeNode1
+            right = treeNode3
+        }
+        val treeNode7 = TreeNode(7).apply {
+            left = treeNode6
+            right = treeNode9
+        }
+        val treeNode4 = TreeNode(4).apply {
+            left = treeNode2
+            right = treeNode7
+        }
+        assertEquals(9, binaryTreeMaxValue.solve(treeNode4))
+    }
+}
